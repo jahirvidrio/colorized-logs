87cdf0f90efdb75818ba6ef7bad8c1ef82482d62
Jahir Vidrio <jahir.vidrio@gmail.com>
2021-06-12 22:29:03 -0500

    refactor: Exports with export default

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,35 @@
 import colors from 'colors/safe';
 
-
-export default {
-  print(...args: any[]): void {
-    console.log(colors.reset(args.join(' ')));
-  },
-
-  info(...args: any[]): void {
-    console.info(colors.bold(colors.blue(' [ INFO ] \t')), colors.blue(args.join(' ')));
-  },
-
-  test(...args: any[]): void {
-    console.info(colors.bold(colors.bgMagenta(colors.white(' [ TEST ] \t'))), colors.magenta(args.join(' ')));
-  },
-
-  error(...args: any[]): void {
-    console.error(colors.bold(colors.bgRed(colors.white(' [ ERROR ] \t'))), colors.bold(colors.red(args.join(' '))));
-  },
-
-  warn(...args: any[]): void {
-    console.warn(colors.bold(colors.yellow(' [ WARNING ] \t')), colors.bold(colors.yellow(args.join(' '))));
-  },
+type ColorizedLogger = {
+  print(...args: any[]): void;
+  info(...args: any[]): void;
+  test(...args: any[]): void;
+  error(...args: any[]): void;
+  warn(...args: any[]): void;
 };
+
+
+export function print(...args: any[]): void {
+  console.log(colors.reset(args.join(' ')));
+}
+
+export function info(...args: any[]): void {
+  console.info(colors.bold(colors.blue(' [ INFO ] \t')), colors.blue(args.join(' ')));
+}
+
+export function test(...args: any[]): void {
+  console.info(colors.bold(colors.bgMagenta(colors.white(' [ TEST ] \t'))), colors.magenta(args.join(' ')));
+}
+
+export function error(...args: any[]): void {
+  console.error(colors.bold(colors.bgRed(colors.white(' [ ERROR ] \t'))), colors.bold(colors.red(args.join(' '))));
+}
+
+export function warn(...args: any[]): void {
+  console.warn(colors.bold(colors.yellow(' [ WARNING ] \t')), colors.bold(colors.yellow(args.join(' '))));
+}
+
+const colorizedLogger: ColorizedLogger = { print, info, test, error, warn }
+
+
+export default colorizedLogger;
